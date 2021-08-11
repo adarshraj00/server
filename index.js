e45bc32d58090d77d356d58eef9166a3cad6b7bb
adarshraj00 <rajadarshofficial@gmail.com>
2021-08-11 14:32:45 +0530

    added atlas

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const cors=require('cors')
-mongoose.connect('mongodb://localhost:27017/lol', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connection.on('connected',()=>{
-    console.log("connected to mongo");
-})
-mongoose.connection.on('error',(err)=>{
-    if(err){
-        console.log("error on connecting to db");
-    }
-})
+
+mongoose.connect("mongodb+srv://adarsh-admin:AoUJo2luTwjrCDHv@cluster0.jjs5s.mongodb.net/lol", {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:27017/lol', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connection.on('connected',()=>{
+//     console.log("connected to mongo");
+// })
+// mongoose.connection.on('error',(err)=>{
+//     if(err){
+//         console.log("error on connecting to db");
+//     }
+// })
 
 const PORT=3000;
 const app=express();
@@ -18,6 +20,7 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('ngApp'));
 app.use('/api',api);
 
 
@@ -28,3 +31,7 @@ app.get('/',(req,res)=>{
 app.listen(PORT,()=>{
     console.log(`Server is running at http://localhost:${PORT}`);
 })
+
+
+//mongo "mongodb+srv://cluster0.jjs5s.mongodb.net/myFirstDatabase" --username adarsh-admin
+// to connect via terminal
