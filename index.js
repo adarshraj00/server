0eeaaae6b187984d195dbf2716809db7a5c4b47d
adarshraj00 <rajadarshofficial@gmail.com>
2021-08-12 09:52:29 +0530

    t

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express=require('express');
 const mongoose=require('mongoose');
 const cors=require('cors')
 
-mongoose.connect("mongodb+srv://adarsh-admin:AoUJo2luTwjrCDHv@cluster0.jjs5s.mongodb.net/lol", {useNewUrlParser: true, useUnifiedTopology: true});
-// mongoose.connect('mongodb://localhost:27017/lol', {useNewUrlParser: true, useUnifiedTopology: true});
-// mongoose.connection.on('connected',()=>{
-//     console.log("connected to mongo");
-// })
-// mongoose.connection.on('error',(err)=>{
-//     if(err){
-//         console.log("error on connecting to db");
-//     }
-// })
+//mongoose.connect("mongodb+srv://adarsh-admin:AoUJo2luTwjrCDHv@cluster0.jjs5s.mongodb.net/lol", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/lol', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.on('connected',()=>{
+    console.log("connected to mongo");
+})
+mongoose.connection.on('error',(err)=>{
+    if(err){
+        console.log("error on connecting to db");
+    }
+})
 
-const PORT=process.env.PORT || 8080;
+const PORT=process.env.PORT || 3000;
 const app=express();
 const api=require('./routes/api');
 app.use(cors());
